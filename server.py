2a84d6a0033de91c64ea44ff0c966d02ffa82abf
Agustin <agustin.andrews@gmail.com>
2021-02-13 18:04:42 -0300

    first commit

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
+
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -7,8 +10,15 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    strawberry = request.form["strawberry"]
+    raspberry = request.form["raspberry"]
+    apple = request.form["apple"]
+    total = int(strawberry) + int(raspberry) + int(apple)
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    student_id = request.form["student_id"]
+    datetime_in_checkout = datetime.now().strftime("%b %d %G %H:%M:%S %p.")
+    return render_template("checkout.html", strawberry=strawberry,raspberry=raspberry,apple=apple, first_name=first_name,last_name=last_name,student_id=student_id,total=total, datetime_in_checkout=datetime_in_checkout)
 
 @app.route('/fruits')         
 def fruits():
